829624369e91f316160c294c5023f2bae685d0ef
GeonHeeLeee <ghlee00125@naver.com>
2024-01-17 21:08:31 +0900

    프로젝트 초기 세팅

---
+++ realWorld/src/test/java/GDSC/realWorld/RealWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package GDSC.realWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
